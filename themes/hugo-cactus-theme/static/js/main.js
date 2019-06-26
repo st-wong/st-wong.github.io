75aae2c05127e5a341431e54aec249a061717220
st-wong <shintatt.wong@gmail.com>
2019-06-26 23:59:35 +0800

    Fix pixel fun theme to be consistent during navigation and remove unused files

--- themes/hugo-cactus-theme/static/js/main.js
+++ themes/hugo-cactus-theme/static/js/main.js
@@ -1,36 +1,29 @@
-// To make images retina, add a class "2x" to the img element
-// and add a <image-name>@2x.png image. Assumes jquery is loaded.
- 
-function isRetina() {
-	var mediaQuery = "(-webkit-min-device-pixel-ratio: 1.5),\
-					  (min--moz-device-pixel-ratio: 1.5),\
-					  (-o-min-device-pixel-ratio: 3/2),\
-					  (min-resolution: 1.5dppx)";
- 
-	if (window.devicePixelRatio > 1)
-		return true;
- 
-	if (window.matchMedia && window.matchMedia(mediaQuery).matches)
-		return true;
- 
-	return false;
-};
- 
- 
-function retina() {
-	
-	if (!isRetina())
-		return;
-	
-	$("img.2x").map(function(i, image) {
-		
-		var path = $(image).attr("src");
-		
-		path = path.replace(".png", "@2x.png");
-		path = path.replace(".jpg", "@2x.jpg");
-		
-		$(image).attr("src", path);
-	});
-};
- 
-$(document).ready(retina);
+function togglePixelFont() {
+	const htmlTag = document.getElementsByTagName("html")[0];
+    if (htmlTag.hasAttribute("data-theme")) {
+		if (typeof(Storage) !== "undefined") {
+			sessionStorage.pixelFontToggle = 0;
+		}
+        htmlTag.removeAttribute("data-theme");
+        return
+    }
+	htmlTag.setAttribute("data-theme", "pixel")
+	if (typeof(Storage) !== "undefined") {
+		sessionStorage.pixelFontToggle = 1;
+	}
+}
+
+function setPixelFont(){
+	if (typeof(Storage) !== "undefined" && sessionStorage.pixelFontToggle) {
+		console.log(sessionStorage.getItem("pixelFontToggle"));
+		const htmlTag = document.getElementsByTagName("html")[0];
+		if (Number(sessionStorage.getItem("pixelFontToggle")) === 1) {
+			document.getElementById("pixelfont-toggle").checked = true;
+			htmlTag.setAttribute("data-theme", "pixel");
+		}
+	}
+}
+
+document
+    .getElementById("pixelfont-toggle")
+	.addEventListener("click", togglePixelFont);
